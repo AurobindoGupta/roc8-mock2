6e7fb6ab90e4eb0c487a92504ba47583270db25d
AurobindoGupta <aurogpt10@gmail.com>
2021-06-27 11:47:47 +0530

    initial display, json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from "./ProductList";
+import Cart from "./Cart";
+import {Route, BrowserRouter as Router,Link, Switch} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+     <nav style={{backgroundColor:"blue"}}>
+    <Switch>
+    <Route path='/cart' component={Cart}></Route>
+     <div className="navbar">
+      <img className="logo" src='flipkart.png' height="25" alt=""></img>
+      <input className="search"placeholder="search"></input>
+        <div className="nav-links">
+          <ul>
+            <li><button>Login</button></li>
+            <li><button>More</button></li>
+            <li><button><Link to="/cart">cart</Link></button></li>
+          </ul>
+        </div>
+      </div>
+        </Switch>
+     </nav>
+          <div>
+            <ProductList/>
+          </div>
+    </Router> 
     </div>
   );
 }
